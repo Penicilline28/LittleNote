0cbd3225a13753f93995fe4e136d76150ae1e0e0
Noël Macé <contact@noelmace.com>
2014-12-04 09:20:25 +0100

    Version alternative
    à base de spannables

--- LittleNote/gen/com/noelmace/android/demo/littlenote/R.java
+++ LittleNote/gen/com/noelmace/android/demo/littlenote/R.java
@@ -24,15 +24,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int ic_send_button=0x7f020001;
+        public static final int ic_noeffect=0x7f020001;
+        public static final int ic_send_button=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
+        public static final int action_settings=0x7f090007;
         public static final int bold_button=0x7f090001;
         public static final int buttons_layout=0x7f090000;
         public static final int italic_button=0x7f090002;
-        public static final int note_view=0x7f090004;
-        public static final int show_button=0x7f090005;
+        public static final int noeffect_button=0x7f090004;
+        public static final int note_view=0x7f090005;
+        public static final int show_button=0x7f090006;
         public static final int underline_button=0x7f090003;
     }
     public static final class integer {
@@ -49,6 +51,7 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int bold_text=0x7f060003;
         public static final int italic_text=0x7f060004;
+        public static final int noeffect_text=0x7f060007;
         public static final int note_hint=0x7f060002;
         public static final int send_button_desc=0x7f060006;
         public static final int underline_text=0x7f060005;
--- LittleNote/src/com/noelmace/android/demo/littlenote/MainActivity.java
+++ LittleNote/src/com/noelmace/android/demo/littlenote/MainActivity.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.CharacterStyle;
+import android.text.style.StyleSpan;
+import android.text.style.SubscriptSpan;
+import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-
 
 public class MainActivity extends Activity {
 
@@ -15,60 +21,36 @@ public class MainActivity extends Activity {
 	private static final String IS_BOLD_KEY = "com.noelmace.android.demo.littlenote.boldMode";
 	private static final String IS_ITALIC_KEY = "com.noelmace.android.demo.littlenote.italicMode";
 	private static final String IS_UNDERLINED_KEY = "com.noelmace.android.demo.littlenote.underlineMode";
-	
-	//private static final String LOG_TAG = "com.noelmace.android.demo.littlenote.debug";
-	
+
+	 private static final String LOG_TAG =
+	 "com.noelmace.android.demo.littlenote.warning";
+
 	private EditText noteView;
-	private String noteHtmlText;
-	
+
 	private boolean isBold;
 	private boolean isItalic;
 	private boolean isUnderlined;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        noteView = (EditText) findViewById(R.id.note_view);
-        
-        if(savedInstanceState != null){
-        	noteHtmlText = savedInstanceState.getString(NOTE_HTML_KEY);
-        	noteView.setText(Html.fromHtml(noteHtmlText));
-        	isBold = savedInstanceState.getBoolean(IS_BOLD_KEY);
-        	isItalic = savedInstanceState.getBoolean(IS_ITALIC_KEY);
-        	isUnderlined = savedInstanceState.getBoolean(IS_UNDERLINED_KEY);
-            updateText();
-        } else {
-        	isBold = false;
-        	isItalic = false;
-        	isUnderlined = false;
-        	noteHtmlText = "";
-        }
-        
-        //noteView.addTextChangedListener(textWatcher);
-    }
 
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-*/
+		noteView = (EditText) findViewById(R.id.note_view);
+
+		if (savedInstanceState != null) {
+			noteView.setText(Html.fromHtml(savedInstanceState
+					.getString(NOTE_HTML_KEY)));
+			isBold = savedInstanceState.getBoolean(IS_BOLD_KEY);
+			isItalic = savedInstanceState.getBoolean(IS_ITALIC_KEY);
+			isUnderlined = savedInstanceState.getBoolean(IS_UNDERLINED_KEY);
+		} else {
+			isBold = false;
+			isItalic = false;
+			isUnderlined = false;
+		}
+
+	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
@@ -78,57 +60,112 @@ public class MainActivity extends Activity {
 		outState.putBoolean(IS_UNDERLINED_KEY, isUnderlined);
 		super.onSaveInstanceState(outState);
 	}
-	
+
 	// TODO : unifier, simplifier
 
-	public void bold(View view){
-		noteHtmlText = Html.toHtml(noteView.getText());
-		if(!isBold){
-			noteHtmlText = noteHtmlText.substring(0, noteHtmlText.length() - 5) + "<b>{}</b>" + noteHtmlText.substring(noteHtmlText.length() - 5);
-		}else{
+	public void changeStyle(View view) {
+		int start = noteView.getSelectionStart();
+		int end = noteView.getSelectionEnd();
+		SpannableStringBuilder ssb = new SpannableStringBuilder(noteView.getText());
+		SpannableStringBuilder subSsb = new SpannableStringBuilder(ssb, start, end);
+		CharacterStyle style = new StyleSpan(android.graphics.Typeface.NORMAL);
+		
+		
+		if(view.getId() == R.id.noeffect_button){
+			CharacterStyle[] toRemoveSpans = subSsb.getSpans(0, subSsb.length(), CharacterStyle.class);
+			Log.d(LOG_TAG, ssb.toString());
+			for(int i = 0; i < toRemoveSpans.length; i++){
+				ssb.removeSpan(toRemoveSpans[i]);
+			}
+		}
+		switch (view.getId()) {
+			case R.id.bold_button:
+				style = new StyleSpan(android.graphics.Typeface.BOLD);
+				break;
+			case R.id.italic_button:
+				style = new StyleSpan(android.graphics.Typeface.ITALIC);
+				break;
+			case R.id.underline_button:
+				style = new UnderlineSpan();
+				break;
+			case R.id.noeffect_button:
+				style = null;
+				break;
+			default:
+				Log.w(LOG_TAG, view.getId() + "is not implemented");
+				break;
+		}
+		
+		if(start != end){
+			ssb.setSpan(style, start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		} else {
+			// TODO
+		}
+
+		noteView.setText(ssb);
+		noteView.setSelection(start, end);
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.showSoftInput(noteView, InputMethodManager.SHOW_IMPLICIT);
+	}
+
+	/*
+	public void bold(View view) {
+
+		if (!isBold) {
+			// noteHtmlText = noteHtmlText.substring(0, noteHtmlText.length() -
+			// 5) + "<b>{}</b>" + noteHtmlText.substring(noteHtmlText.length() -
+			// 5);
+		} else {
 			noteHtmlText = noteHtmlText.replace("{", "");
 			noteHtmlText = noteHtmlText.replace("}", "");
 		}
 		isBold = !isBold;
 		updateText();
 	}
-	
-	public void italic(View view){
+
+	public void italic(View view) {
 		noteHtmlText = Html.toHtml(noteView.getText());
-		if(!isItalic){
-			noteHtmlText = noteHtmlText.substring(0, noteHtmlText.length() - 5) + "<i>{}</i>" + noteHtmlText.substring(noteHtmlText.length() - 5);
-		}else{
+		if (!isItalic) {
+			noteHtmlText = noteHtmlText.substring(0, noteHtmlText.length() - 5)
+					+ "<i>{}</i>"
+					+ noteHtmlText.substring(noteHtmlText.length() - 5);
+		} else {
 			noteHtmlText = noteHtmlText.replace("{", "");
 			noteHtmlText = noteHtmlText.replace("}", "");
 		}
 		isItalic = !isItalic;
 		updateText();
 	}
-	
-	public void underline(View view){
+
+	public void underline(View view) {
 		noteHtmlText = Html.toHtml(noteView.getText());
-		if(!isUnderlined){
-			noteHtmlText = noteHtmlText.substring(0, noteHtmlText.length() - 5) + "<u>{}</u>" + noteHtmlText.substring(noteHtmlText.length() - 5);
-		}else{
+		if (!isUnderlined) {
+			noteHtmlText = noteHtmlText.substring(0, noteHtmlText.length() - 5)
+					+ "<u>{}</u>"
+					+ noteHtmlText.substring(noteHtmlText.length() - 5);
+		} else {
 			noteHtmlText = noteHtmlText.replace("{", "");
 			noteHtmlText = noteHtmlText.replace("}", "");
 		}
 		isUnderlined = !isUnderlined;
 		updateText();
 	}
-	
-	public void send(View view){
+	*/
+
+	public void send(View view) {
 		// TODO
 	}
-	
-	protected void updateText(){
+
+	/*
+	protected void updateText() {
 		noteView.setText(Html.fromHtml(noteHtmlText));
-		int pos = noteView.getText().length() -2;
-		if(isBold || isItalic || isUnderlined){
+		int pos = noteView.getText().length() - 2;
+		if (isBold || isItalic || isUnderlined) {
 			pos -= 1;
 		}
 		noteView.setSelection(pos);
 		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 		imm.showSoftInput(noteView, InputMethodManager.SHOW_IMPLICIT);
 	}
+	*/
 }
